36a07d63d7aca657fe17dedffc7b3703bfce313c
Trupti16042004 <truptipardeshi91@gmail.com>
2025-02-07 10:04:59 +0530

    Odd_Number
    To Check Odd Number Between 1 to 100

---
+++ Odd_Number.java
@@ -0,0 +1,15 @@
+class Even_Number
+{
+	public static void main(String[] args) {
+	
+
+		System.out.println("Odd Number are : ");
+		for(int i=1;i<=100;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i+" ");
+			}
+		}
+	}
+}
