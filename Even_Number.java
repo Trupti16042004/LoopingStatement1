b6bb4f3ee34715c8c7f4b9d1ed4a3b26c2312d18
Trupti16042004 <truptipardeshi91@gmail.com>
2025-02-07 10:03:36 +0530

    Even_Number
    To check Even Number  Between the range 1 to 100

---
+++ Even_Number.java
@@ -0,0 +1,16 @@
+class Even_Number
+{
+	public static void main(String[] args) {
+	
+		System.out.println("Even number are : ");	
+		for(int i=1;i<=100;i++)
+		{
+			if(i%2==0 )
+			{
+				System.out.print(i+"  ");
+			}
+		}
+		
+
+	}
+}
